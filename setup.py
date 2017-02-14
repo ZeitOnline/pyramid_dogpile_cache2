df3dc9b8af705b703d06fe5071e5d21569147495
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-02-14 08:44:14 +0100

    Back to development: 1.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def project_path(*names):
 
 setup(
     name='pyramid_dogpile_cache2',
-    version='1.0.3',
+    version='1.0.4.dev0',
 
     install_requires=[
         'Beaker',  # For parsing pylibmc behaviors from ini file.
